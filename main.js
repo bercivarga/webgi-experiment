095e34b189f333b6ea027435bf2173ff34a0f3d9
bercivarga <65171545+bercivarga@users.noreply.github.com>
2023-01-08 21:55:21 +0100

    wip

--- main.js
+++ main.js
@@ -1,19 +1,94 @@
 import './style.css'
-import * as THREE from 'three';
 
-const canvas = document.getElementById('canvas');
+import {
+    ViewerApp,
+    AssetManagerPlugin,
+    GBufferPlugin,
+    timeout,
+    ProgressivePlugin,
+    TonemapPlugin,
+    SSRPlugin,
+    SSAOPlugin,
+    DiamondPlugin,
+    FrameFadePlugin,
+    GLTFAnimationPlugin,
+    GroundPlugin,
+    BloomPlugin,
+    TemporalAAPlugin,
+    AnisotropyPlugin,
+    GammaCorrectionPlugin,
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ canvas });
+    addBasePlugins,
+    TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+    // Color, // Import THREE.js internals
+    // Texture, // Import THREE.js internals
+} from "webgi";
 
-camera.position.z = 5;
+async function setupViewer(){
 
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+    // Initialize the viewer
+    const viewer = new ViewerApp({
+        canvas: document.getElementById('canvas'),
+        useRgbm: false,
+        isAntialiased: false,
+
+    });
+
+    const camera = viewer.scene.activeCamera;
+    camera.controls.enabled = false;
+    viewer.scene.activeCamera = viewer.createCamera(camera.cameraObject);
+
+    const camPos = camera.position;
+    const camTarget = camera.target;
+
+    // Add some plugins
+    const manager = await viewer.addPlugin(AssetManagerPlugin)
+
+    // Add a popup(in HTML) with download progress when any asset is downloading.
+    // await viewer.addPlugin(AssetManagerBasicPopupPlugin)
+
+    // Add plugins individually.
+    // await viewer.addPlugin(GBufferPlugin)
+    // await viewer.addPlugin(new ProgressivePlugin(32))
+    await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
+    // await viewer.addPlugin(GammaCorrectionPlugin)
+    await viewer.addPlugin(SSRPlugin)
+    await viewer.addPlugin(SSAOPlugin)
+    // await viewer.addPlugin(DiamondPlugin)
+    // await viewer.addPlugin(FrameFadePlugin)
+    // await viewer.addPlugin(GLTFAnimationPlugin)
+    // await viewer.addPlugin(GroundPlugin)
+    await viewer.addPlugin(BloomPlugin)
+    // await viewer.addPlugin(TemporalAAPlugin)
+    // await viewer.addPlugin(AnisotropyPlugin)
+
+    // or use this to add all main ones at once.
+    await addBasePlugins(viewer)
+
+    // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
+    await viewer.addPlugin(CanvasSnipperPlugin)
+
+    // This must be called once after all plugins are added.
+    viewer.renderer.refreshPipeline()
+
+    await manager.addFromPath("chair-scene.glb")
+
+    // Load an environment map if not set in the glb file
+    // await viewer.scene.setEnvironment(
+    //     await manager.importer!.importSinglePath<ITexture>(
+    //         "./assets/environment.hdr"
+    //     )
+    // );
+
+    // Add some UI for tweak and testing.
+    // const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
+    // Add plugins to the UI to see their settings.
+    // uiPlugin.setupPlugins(TonemapPlugin, CanvasSnipperPlugin)
+
+    return viewer;
 }
 
-animate();
+setupViewer().then((viewer) => {
+    viewer.scene.activeCamera.controls.enabled = false
+});
